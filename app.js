9600e7c4977c7ffd81b8d3ef14ef42274a9211f5
harshsing25 <harshaiactr@gmail.com>
2020-05-28 12:47:07 +0530

    basic layout

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+let port= process.env.PORT ;
+if (port==null||port==""){
+    port=3000;
+}
+
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/create", function(req, res){
+      res.render("create");
+  
+});
+
+
+app.get("/about", function(req, res){
+
+  
+    res.render("about");
+});
+
+app.get("/contact", function(req, res){
+  
+    res.render("contact");
+});
+
+
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
